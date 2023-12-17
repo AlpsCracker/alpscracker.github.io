f34697cdf157ab05540d5cab303a8d75fcd38b57
AlpsCracker <1355209980@qq.com>
2023-12-17 23:50:52 +0800

    commit my site

--- BuildMySite.ps1
+++ BuildMySite.ps1
@@ -1,4 +1,4 @@
-jekyll build
+jekyll server
 Copy-Item -Path "D:\Coding_Space\AnnualSummary\AlvinBlog\_site\*" -Destination "D:\Coding_Space\AnnualSummary\alpscracker.github.io" -Recurse -Force
 cd D:\Coding_Space\AnnualSummary\alpscracker.github.io
 git add .
