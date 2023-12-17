b6c3afd9f0736f76c6d72fb41e259bac3a2ed4f8
AlpsCracker <1355209980@qq.com>
2023-12-18 00:48:46 +0800

    commit my site

---
+++ BuildMySite.ps1
@@ -0,0 +1,7 @@
+jekyll build
+Get-ChildItem -Path "D:\Coding_Space\AnnualSummary\alpscracker.github.io" -Exclude .git, README.md | Remove-Item -Recurse -Force
+Copy-Item -Path "D:\Coding_Space\AnnualSummary\AlvinBlog\_site\*" -Destination "D:\Coding_Space\AnnualSummary\alpscracker.github.io" -Recurse -Force
+Set-Location D:\Coding_Space\AnnualSummary\alpscracker.github.io\
+git add .
+git commit -m "commit my site"
+git push
