370dec32befec8dbcff67550b32f33ef4ac82b99
AlpsCracker <1355209980@qq.com>
2024-03-16 01:06:12 +0800

    commit my site

--- BuildMySite.ps1
+++ BuildMySite.ps1
@@ -1,7 +1,7 @@
 jekyll build
-Get-ChildItem -Path "D:\Coding_Space\AnnualSummary\alpscracker.github.io" -Exclude .git, README.md | Remove-Item -Recurse -Force
-Copy-Item -Path "D:\Coding_Space\AnnualSummary\AlvinBlog2\_site\*" -Destination "D:\Coding_Space\AnnualSummary\alpscracker.github.io" -Recurse -Force
-Set-Location D:\Coding_Space\AnnualSummary\alpscracker.github.io\
+Get-ChildItem -Path "..\alpscracker.github.io" -Exclude .git, README.md | Remove-Item -Recurse -Force
+Copy-Item -Path ".\_site\*" -Destination "..\alpscracker.github.io" -Recurse -Force
+Set-Location ..\alpscracker.github.io\
 git add .
 git commit -m "commit my site"
 git push
